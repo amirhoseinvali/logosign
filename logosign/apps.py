65adfd60fca7a8ef57cd2cc3e92c125f2529e2a8
AmirhoseinVali <amirhosein.vali@yahoo.com>
2025-07-26 10:11:06 +0330

    handle gunicorn and uwsgi

--- logosign/apps.py
+++ logosign/apps.py
@@ -5,7 +5,13 @@ class LogoSignConfig(AppConfig):
     name = "logosign"
 
     def ready(self):
-        if "runserver" in sys.argv and os.environ.get("RUN_MAIN") == "true":
+        argv = ' '.join(sys.argv).lower()
+        valid_commands = [
+            "runserver",
+            "gunicorn",
+            "uwsgi",
+        ]
+        if any(cmd in argv for cmd in valid_commands) and os.environ.get("RUN_MAIN") == "true":
             logosign_path = os.path.join(os.getcwd(), "logosign.ascii")
             if not os.path.exists(logosign_path):
                 logosign_path = os.path.join(os.path.dirname(__file__), "default.ascii")
